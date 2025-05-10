5355199c1b496ea44d0272e104fe01a6de27186a
BAPPAYNE <sahilkatrodiyalwt@gmail.com>
2025-05-10 18:51:06 +0530

    Refactor server setup into RunMathemegic function
    Encapsulated server setup and route registration in a new function called `RunMathemegic()`. The `main()` function now calls this new function and returns a termination message. Restored necessary header inclusions and corrected the CSS file path. Improved overall code structure for better organization and readability.

--- Mathemegic/src/main.cpp
+++ Mathemegic/src/main.cpp
@@ -1,65 +1,32 @@
-//#include <crow.h>
-//#include "../include/web_routes.h"
-//#include <iostream>
-//
-//using namespace std;
-//
-//int main() {
-//    crow::SimpleApp app;
-//
-//    std::cout << "Registering routes...\n";
-//    CROW_ROUTE(app, "/")(WebRoutes::home_page);
-//    CROW_ROUTE(app, "/prime-checker")
-//        .methods("GET"_method, "POST"_method)(WebRoutes::prime_checker);
-//    CROW_ROUTE(app, "/rsa-generator")
-//        .methods("GET"_method, "POST"_method)(WebRoutes::rsa_generator);
-//    CROW_ROUTE(app, "/random_prime_generator")
-//        .methods("GET"_method, "POST"_method)(WebRoutes::random_prime_generator);
-//    CROW_ROUTE(app, "/random_rsa_generator")
-//        .methods("GET"_method, "POST"_method)(WebRoutes::random_rsa_generator);
-//
-//    CROW_ROUTE(app, "/style.css")([]() {
-//        std::ifstream file("../static/style.css", ios::binary);
-//        if (!file.is_open()) {
-//            return crow::response(404, "CSS file not found");
-//        }
-//        std::stringstream buffer;
-//        buffer << file.rdbuf();
-//        crow::response res(buffer.str());
-//        res.set_header("Content-Type", "text/css");
-//        return res;
-//        });
-//
-//
-//    std::cout << "Starting server on port 18080...\n";
-//    app.port(18080).multithreaded().run();
-//    std::cout << "Server stopped.\n";
-//    return 0;
-//}
-
-
 #include <crow.h>
 #include "../include/web_routes.h"
 #include <iostream>
 #include <fstream>
 #include <sstream>
 
-int main() {
+using namespace std;
+
+string RunMathemegic(){
+
     crow::SimpleApp app;
 
     std::cout << "Registering routes...\n";
     CROW_ROUTE(app, "/")(WebRoutes::home_page);
+
     CROW_ROUTE(app, "/prime-checker")
         .methods("GET"_method, "POST"_method)(WebRoutes::prime_checker);
+
     CROW_ROUTE(app, "/rsa-generator")
         .methods("GET"_method, "POST"_method)(WebRoutes::rsa_generator);
+
     CROW_ROUTE(app, "/random_prime_generator")
         .methods("GET"_method, "POST"_method)(WebRoutes::random_prime_generator);
+
     CROW_ROUTE(app, "/random_rsa_generator")
         .methods("GET"_method, "POST"_method)(WebRoutes::random_rsa_generator);
 
     CROW_ROUTE(app, "/style.css")([]() {
-        std::ifstream file("../../Mathemegic/static/style.css", std::ios::binary);
+        std::ifstream file("../static/style.css", std::ios::binary);
         if (!file.is_open()) {
             std::cerr << "Failed to open static/style.css\n";
             return crow::response(404, "CSS file not found");
@@ -88,5 +55,11 @@ int main() {
     std::cout << "Starting server on port 18080...\n";
     app.port(18080).multithreaded().run();
     std::cout << "Server stopped.\n";
+
+    return "Terminated.";
+}
+
+int main() {
+    RunMathemegic();
     return 0;
 }
